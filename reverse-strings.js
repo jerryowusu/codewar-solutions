2d4b8e2c784b7a32f53538836354c824ce899635
Jerry Owusu <jerryowusu50@yahoo.com>
2023-03-08 10:05:14 -0500

    Create reverse-strings.js

---
+++ reverse-strings.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// STRINGSFUNDAMENTALS
+
+
+Ruby 
+
+def solution(str)
+  str.reverse()
+end
+
+
+// Javascript
+
+function solution(str){   
+    return str.split('').reverse().join('');  
+}
+
+
