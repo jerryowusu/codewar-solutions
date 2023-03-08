3c87d91fe2bccf0c93a844a12fa4ae077be64020
Jerry Owusu <jerryowusu50@yahoo.com>
2023-03-08 09:50:28 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,29 @@
-1
+## DNA strands
+
+1 function DNAStrand(dna){
+  let result = ''
+  
+  for (let i=0; i<dna.length; i++) {
+    if (dna[i] === "A") result += "T"
+    if (dna[i] === "T") result += "A"
+    if (dna[i] === "C") result += "G"
+    if (dna[i] === "G") result += "C"
+  }
+  return result
+}
+
+
+function DNAStrand(dna){
+  //your code here
+  const dnaStrand = {
+    'A' : 'T',
+    'T' : 'A',
+    'C' : 'G',
+    'G' : 'C'
+  }
+  
+  return dna.split('').map(elem => dnaStrand[elem]).join('')
+  
+}
+
+
