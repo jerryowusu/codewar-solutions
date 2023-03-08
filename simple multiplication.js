57f43e116ec34ce97e79ed106146dd756b79a4b5
Jerry Owusu <jerryowusu50@yahoo.com>
2023-03-08 10:16:41 -0500

    Create simple multiplication.js

---
+++ simple multiplication.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// FUNDAMENTALS
+
+javascript
+
+function simpleMultiplication(number) {
+    // your code........
+return number % 2 === 0 ?( number * 8 ): (number * 9)
+}
